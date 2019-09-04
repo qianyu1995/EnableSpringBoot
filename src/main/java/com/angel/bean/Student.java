18d29b565cb05d4da9ffc56193d4df87e1d4cd01
陈明 <cmingming@extremevalue.cn>
2019-09-04 18:29:21 +0800

    #docker-compose测试

--- src/main/java/com/angel/bean/Student.java
+++ src/main/java/com/angel/bean/Student.java
@@ -5,9 +5,13 @@ import java.io.Serializable;
 public class Student implements Serializable{
 	
 	private static final long serialVersionUID = -339516038496531943L;
+	
 	private String sno;
+	
 	private String name;
+	
 	private String sex;
+	
 	public String getSno() {
 		return sno;
 	}
